2e2c4276af7b50e885402d2031f979fb38b7ce70
annameless <annameless@gmail.com>
2018-04-24 21:15:42 +1200

    Initial commit

---
+++ Fundamentals/Introduction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name, please...");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+
+            var hours = FigureOutLevelOfSleepiness();
+
+            if (hours >= 8)
+                Console.WriteLine("You are well rested");
+            else
+                Console.WriteLine("You need some sleep");
+        }
+
+        private static decimal FigureOutLevelOfSleepiness(){
+            Console.WriteLine("How many hours of sleep did you get last night...");
+            decimal hours;
+            var isSuccess = decimal.TryParse(Console.ReadLine(), out hours);
+            if(!isSuccess) {
+                Console.WriteLine("Please enter a number");
+                return FigureOutLevelOfSleepiness();
+            }
+            return hours;
+        }
+    }
+}
