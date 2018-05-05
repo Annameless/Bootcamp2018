815036fc7e584428a047bc17b2b1d55a0a66a8b8
annameless <annameless@gmail.com>
2018-05-05 14:55:52 +1200

    Enumerators in C# Collection Fundamentals

---
+++ DotNetBootcamp/Enumerators/AllDaysOfWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Enumerators
+{
+    public class AllDaysOfWeek : IEnumerable<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            Console.WriteLine("Calling generic GetEnumerator");
+            yield return "Monday";
+            yield return "Tuesday";
+            yield return "Wednesday";
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DotNetBootcamp/Enumerators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enumerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Will enumerate elements of the array");
+            string[] daysOfWeek = {
+                                      "Monday",
+                                      "Tuesday",
+                                      "Wednesday",
+                                      "Thursday",
+                                      "Friday",
+                                      "Saturday",
+                                      "Sunday"
+                                  };
+            DisplayItems(daysOfWeek);
+
+            Console.WriteLine("\nWill enumerate the characters in the string");
+            DisplayItems("Hello, World!");
+
+            Console.WriteLine("\nImplementing own enumerator");
+            var alldays = new AllDaysOfWeek();
+            foreach(var d in alldays){
+                Console.WriteLine(d);
+            }
+        }
+
+        static void DisplayItems<T>(IEnumerable<T> collection){
+            using(var enumerator = collection.GetEnumerator()){
+                var hasNext = enumerator.MoveNext();
+                while(hasNext){
+                    Console.WriteLine(enumerator.Current);
+                    hasNext = enumerator.MoveNext();
+                }
+            }
+        }
+    }
+}
