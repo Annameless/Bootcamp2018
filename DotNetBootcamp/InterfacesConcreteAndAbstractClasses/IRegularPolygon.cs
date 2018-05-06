0057f0deb14547c621dda69a719e09aa154760dd
annameless <annameless@gmail.com>
2018-05-06 16:06:26 +1200

    Interfaces, Abstract Classes and Concrete classes

---
+++ DotNetBootcamp/InterfacesConcreteAndAbstractClasses/IRegularPolygon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace InterfacesConcreteAndAbstractClasses
+{
+    public interface IRegularPolygon
+    {
+        int NumberOfSides { get; set; }
+        int SideLength { get; set; }
+
+        double GetPerimeter();
+        double GetArea();
+    }
+}
---
+++ DotNetBootcamp/InterfacesConcreteAndAbstractClasses/Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace InterfacesConcreteAndAbstractClasses
+{
+    public class Square : ConcreteRegularPolygon
+    {
+        public Square(int length) :
+            base(4, length)
+        { }
+
+        public override double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+}
