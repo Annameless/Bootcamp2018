2af900108750ed00ef01b6364b37df1eda4f1ffe
annameless <annameless@gmail.com>
2018-05-06 17:54:44 +1200

    Explicit Interface Implementation

---
+++ DotNetBootcamp/ExplicitInterfaceImplementation/Implemetations.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExplicitInterfaceImplementation
+{
+    public class StandardCatalog : ISaveable, IPersistable
+    {
+        public void Load()
+        {
+        }
+
+        public string Save()
+        {
+            return "Catalog Save";
+        }
+    }
+
+    public class ExplicitCatalog : ISaveable, IPersistable
+    {
+        public string Save()
+        {
+            return "Explicit Catalog Save";
+        }
+
+        string ISaveable.Save()
+        {
+            return "ISaveable Save";
+        }
+
+        string IPersistable.Save()
+        {
+            return "IPersistable Save";
+        }
+    }
+
+    public class Catalog : ISaveable, IVoidSaveable
+    {
+        //can have only one standard implicit implemetation, but can more that one explicit implemetations
+        string ISaveable.Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IVoidSaveable.Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EnumerableCatalog : IEnumerable<string>
+    {
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            // all of our code
+            return null;
+        }
+
+        //IEnumerable<T> inherits IEnumerable - so vs implemented both
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DotNetBootcamp/ExplicitInterfaceImplementation/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿namespace ExplicitInterfaceImplementation
+{
+    public interface ISaveable
+    {
+        string Save();
+    }
+
+    public interface IPersistable
+    {
+        string Save();
+    }
+
+    public interface IVoidSaveable
+    {
+        void Save();
+    }
+}
