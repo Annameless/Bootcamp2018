a4c697c7ebf8358832a26ab6f5c22bf27a6cd3de
annameless <annameless@gmail.com>
2018-05-08 22:17:33 +1200

    C# Generic Collections in tests

---
+++ DotNetBootcamp/GenericCollections.Tests/LinkedListTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace GenericCollections.Tests
+{
+    public class LinkedListTests
+    {
+		[Fact]
+        public void Can_Add_After()
+        {
+            var list = new LinkedList<string>();
+            list.AddFirst("Hello");
+            list.AddLast("World");
+            list.AddAfter(list.First, "there");
+
+			Assert.Equal("there", list.First.Next.Value);
+        }
+
+        [Fact]
+        public void Can_Remove_Last()
+        {
+            var list = new LinkedList<string>();
+            list.AddFirst("Hello");
+            list.AddLast("World");
+            list.RemoveLast();
+
+            Assert.Equal(list.First, list.Last);
+        }
+
+        [Fact]
+        public void Can_Find_Items()
+        {
+            var list = new LinkedList<string>();
+            list.AddFirst("Hello");
+            list.AddLast("World");
+
+            Assert.True(list.Contains("Hello"));
+        }
+    }
+}
