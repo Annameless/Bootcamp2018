8e6afc00f6143d78fc902c874b961e2fde41dff6
annameless <annameless@gmail.com>
2018-05-08 22:52:33 +1200

    C# Generic classes and interfaces

---
+++ DotNetBootcamp/DataStructures/Buffer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+	public class Buffer<T> : IBuffer<T>
+	{
+		protected Queue<T> _queue = new Queue<T>();
+
+		public virtual bool IsEmpty => _queue.Count == 0;
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			//return _queue.GetEnumerator();
+			foreach (var item in _queue)
+            {
+                yield return item;
+            }
+		}
+
+		public virtual T Read()
+		{
+			return _queue.Dequeue();
+		}
+
+		public virtual void Write(T value)
+		{
+			_queue.Enqueue(value);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
